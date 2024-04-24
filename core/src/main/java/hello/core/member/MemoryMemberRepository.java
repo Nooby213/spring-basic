f4065fc75f564d6f9b14843612b85fb25c3b2ae1
Nooby213 <lsm5631@naver.com>
2024-04-25 00:50:11 +0900

    회원정보 주문목록

---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryMemberRepository implements MemberRepository {
+    // 동시성 이슈때문에 현업에서는 ConcurrentHashMap<>() 을 쓴다
+    private static final Map<Long, Member> store = new HashMap<>();
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
